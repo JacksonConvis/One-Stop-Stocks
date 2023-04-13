109216bc55bcaf1812b281e399d6276b06dcf02d
JackConvis <jackconvis@gmail.com>
2023-04-13 16:15:06 -0400

    added routing navigation

--- OneDrive/Desktop/One-Stop-Stocks/One-Stop-Frontend/src/app/app.module.ts
+++ OneDrive/Desktop/One-Stop-Stocks/One-Stop-Frontend/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ListOfStocksComponent } from './list-of-stocks/list-of-stocks.component';
 import { FavoritesComponent } from './favorites/favorites.component';
 
 
-
+const routes: Routes = [
+  {path: '' , redirectTo: 'AppComponent', pathMatch: 'full'},
+  { path: 'faves', component: FavoritesComponent },
+  { path: 'stocks', component: ListOfStocksComponent },
+ // { path: 'add', component: AddEventComponent },
+  
+];
 
 @NgModule({
   declarations: [
@@ -17,7 +23,9 @@ import { FavoritesComponent } from './favorites/favorites.component';
     FavoritesComponent,
  
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
